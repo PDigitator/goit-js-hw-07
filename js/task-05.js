cfa426a7adea9f5fb0ce3a53f08772da9a88acb7
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-04-03 11:12:24 +0300

    Deletes old files

--- js/task-05.js
+++
@@ -1,9 +0,0 @@
-// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка "Anonymous".
-
-const inputName = document.querySelector('#name-input');
-const outputName = document.querySelector('#name-output');
-
-const inputNameInputHandler = event => {
-  outputName.textContent = event.currentTarget.value ? event.currentTarget.value : 'Anonymous';
-};
-inputName.addEventListener('input', inputNameInputHandler);
--- js/task-07.js
+++
@@ -1,8 +0,0 @@
-// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
-
-const sizeControle = document.querySelector('#font-size-control');
-const text = document.querySelector('#text');
-
-const sizeControleInputHandler = event => (text.style.fontSize = `${event.currentTarget.value}px`);
-
-sizeControle.addEventListener('input', sizeControleInputHandler);
